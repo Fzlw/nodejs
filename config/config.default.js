980b0ce08e6665f0a0c3a1d3856bbdc136b4733b
liwei <liwei@madingyu.com>
2018-12-13 16:08:21 +0800

    12-13 16:07

--- config/config.default.js
+++ config/config.default.js
@@ -23,7 +23,7 @@ module.exports = appInfo => {
 
     // 映射路径
     config.staticPrefix = 'fz';
-    config.staticVersion = '0.0.4';
+    config.staticVersion = '0.0.5';
 
     // egg 静态资源服务
     config.static = {
