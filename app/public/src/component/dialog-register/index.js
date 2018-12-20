375ae647f25233c1361d0ab203c3698b2f233a38
liwei <liwei@madingyu.com>
2018-12-20 23:00:48 +0800

    todo csrf

--- app/public/src/component/dialog-register/index.js
+++ app/public/src/component/dialog-register/index.js
@@ -5,22 +5,22 @@ define([
     'utils/tools',
     './index.xtpl',
     'utils/dialog'
-], function($, Tools, xtpl, Dialog) {
+], function ($, Tools, xtpl, Dialog) {
 
     class DialogRegister extends Tools {
 
         constructor(userTypeId) {
             super();
-            this.box = $(xtpl());
             this.postData = {
                 userTypeId
             }
             this.render();
+            this.register();
         }
 
         render() {
             let dialog = new Dialog({
-                body: this.box,
+                body: $(xtpl()),
                 width: 360,
                 height: 180,
                 closeBtn: {
@@ -32,6 +32,24 @@ define([
                 closeBtnImg: '//img.cnhash.com/517135e6-5608-437f-95fa-b5363717ac89.svg'
             })
             dialog.open();
+            this.box = $('#' + dialog.id)
+        }
+
+        register() {
+            this.box.find('.register').on('click', () => {
+                let nameArr = this.box.find('.form-register').serializeArray(),
+                    data = {};
+                nameArr.forEach(ele => {
+                    data[ele.name] = ele.value;
+                });
+                // 检查参数 TODO 
+
+                this.post('/register', {
+                    data: Object.assign(this.postData, data)
+                }).then(res => {
+                    console.log(res)
+                }).catch(err => console.log(err))
+            })
         }
 
     }
