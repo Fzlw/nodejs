572e16af0730692298bbd556b49c4a3da10fd067
liwei <liwei@madingyu.com>
2018-12-21 17:06:38 +0800

    test logs

--- config/config.prod.js
+++ config/config.prod.js
@@ -7,7 +7,7 @@ module.exports = appInfo => {
         // 线上日志配置，位于项目nodejs下
         logger: {
             dir: path.join(__dirname, `../../logs/${appInfo.name}`),
-            clearTime: 1 * 24 * 60 * 60    // 定时清除日志  默认1天 单位s  自定义属性
+            clearTime: 10 + 's'    // 定时清除日志  默认1天 单位s  自定义属性 1 * 24 * 60 * 60
         }
     }
 }
