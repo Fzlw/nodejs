5c2e05294c620135f1644aa1f03565d690abdf9a
liwei <liwei@madingyu.com>
2018-12-21 17:30:57 +0800

    logs ok

--- app/schedule/delete_logs.js
+++ app/schedule/delete_logs.js
@@ -1,26 +1,35 @@
 'use strict';
 
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 module.exports = app => {
     return {
         schedule: {
-            interval: app.config.logger.clearTime || '100d',
+            interval: app.config.logger.clearTime || '10s',
             type: 'all'
         },
         async task(ctx) {
             let isOnline = app.config.isOnline;
             // 线上就定期清除日志
             if (!isOnline) return;
-            // ctx.logger.info(app.config.isOnline)
             let dir = app.config.logger.dir;
-            fs.unlink(dir, (err) => {
-                if (err) {
-                    app.logger.error('日志清除错误' + err);
+            // 使用命令行清除日志
+            exec(`rm -rf ${dir}`, (error, stdout, stderr) => {
+                if (error) {
+                    app.logger.warn('WARNNING!!!!  日志清除错误' + err);
                     return;
                 }
-                app.logger.error('日志清除成功' + new Date());
+                app.logger.info('\r----日志清除成功-----\r' + new Date());
             })
+            // 非法操作
+            // fs.unlink(dir, (err) => {
+            //     if (err) {
+            //         app.logger.error('日志清除错误' + err);
+            //         return;
+            //     }
+            //     app.logger.error('日志清除成功' + new Date());
+            // })
         }
     }
 }
